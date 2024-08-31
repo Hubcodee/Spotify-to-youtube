2bbbb3980fda50b717f2c0c7c9ab4e4137d3ff69
Ansh Shrivas <anshshrivas26@gmail.com>
2024-08-31 18:14:52 +0530

    initial code

---
+++ spotipy.py
@@ -0,0 +1,28 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Set up your Spotify credentials
+client_id = 'da6b139e19754cbcb15f6ce202f54746'
+client_secret = '3d00001e4ba14ba68c9a0c7c9af332e3'
+
+# Authenticate
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+# Function to get all song names from a playlist
+def get_playlist_tracks(playlist_id):
+    results = sp.playlist_tracks(playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+    track_names = [track['track']['name'] for track in tracks]
+    return tuple(track_names)
+
+# Spotify playlist URL
+playlist_url = 'https://open.spotify.com/playlist/3DuzHWBRJN4VMir60nB9vn'
+playlist_id = playlist_url.split('/')[-1].split('?')[0]
+
+# Get all song names
+song_names = get_playlist_tracks(playlist_id)
+print(song_names)
